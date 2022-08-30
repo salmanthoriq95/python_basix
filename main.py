63bbbb8d4aa4a1f210e53612018a3c345a9b8f81
Salman Thoriq Al Farisyi <salmanthoriq95@gmail.com>
2022-08-30 17:08:51 +0700

    belajar tipe data dan variable

---
+++ main.py
@@ -0,0 +1,51 @@
+# input dan output
+# nama = input("masukan nama: ")
+# print(nama)
+
+# tipe data, variable
+# angka1 = int(input("masukan assign pertama = "))
+# angka2 = int(input("masukan assign kedua = "))
+# total = angka1 + angka2
+# print("jumlahnya adalah ", total)
+
+# nama_depan = "salman"
+# nama_belakang = "al farisyi 1912 !!!)(%#*(^"
+# umur = 27.5
+# print("nama saya =", nama_depan, nama_belakang)
+# print("umur saya =", umur)
+# print("tipe data umur  =", type(umur))
+
+# name = "salman"
+# age = 17
+# sex = "male"
+# isAccountVerified = False
+
+
+main_char = input("input main char : ")
+mc_weapon = input("input weapen of main char : ")
+mc_power = int(input("total damage main char :"))
+main_enemy = input("input main enemy : ")
+enemy_weapon = input("input weapon main enemy : ")
+enemy_power = int(input("total dmg main enemy : "))
+print(
+    f""" 
+suatu hari {main_char} membawa {mc_weapon} ke hutan, dan bertemu dengan 
+si {main_enemy} yang sedang membawa {enemy_weapon}. karena {main_char} 
+memiliki kekuatan sebesar {mc_power} dan {main_enemy} memiliki
+kekuatan sebesar {enemy_power}, maka siapakah yang kalah? 
+total = main char power - enemy char power
+total = {mc_power - enemy_power}
+kalau semisal total di bawah nol berarti anda kalah
+"""
+)
+
+
+# aritmatika
+# percabangan
+# looping
+# function
+
+# tambahan
+# object
+# class
+# oop
